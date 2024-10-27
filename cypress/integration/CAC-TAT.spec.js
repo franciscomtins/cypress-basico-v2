3b47586be0bd5a07aede98b1e16d99b1dc8ae0ac
Francisco Martins <francisco.mtins@gmail.com>
2024-10-27 17:12:17 -0300

    Aula 13 - Desafio exibir Cat - Invoke

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -204,7 +204,7 @@ describe ('Central de Atendimento ao Cliente TAT', function() {
         .should('have.value', longText)
     })
 
-    it.only('faz uma requisição HTTP', function() {
+    it('faz uma requisição HTTP', function() {
       cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
 
         .should(function(response) {
@@ -214,6 +214,16 @@ describe ('Central de Atendimento ao Cliente TAT', function() {
           expect(statusText).to.equal('OK')
           expect(body).to.include('CAC TAT')
         })
+     })
+
+     it('encontra o gato escondido', function() {
+      cy.get('#cat')
+        .invoke('show')
+        .should('be.visible')
+      cy.get('#title')
+        .invoke('text', 'CAT TAT')
+      cy.get('#subtitle')
+        .invoke('text', 'Easter Egg Cat')
      })
 
 })
